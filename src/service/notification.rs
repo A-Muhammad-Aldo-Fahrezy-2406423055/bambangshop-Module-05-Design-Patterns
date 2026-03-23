794299a02acbc44a0bb53eae83238eda5bc48533
aldofahrezy <aldofhrzy@gmail.com>
2026-03-23 23:21:37 +0700

    Implement notify function in Notification service to notify each Subscriber.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -13,4 +13,18 @@ impl NotificationService {
     pub fn unsubscribe(product_type: &str, url: &str) -> Option<Subscriber> {
         SubscriberRepository::delete(product_type, url)
     }
+
+    pub fn notify(product_type: &str, status: &str, product: Product) {
+        let subscribers = SubscriberRepository::list_all(product_type);
+        for subscriber in subscribers {
+            let payload = Notification {
+                product_title: product.title.clone(),
+                product_type: product.product_type.clone(),
+                product_url: product.get_url(),
+                subscriber_name: subscriber.name.clone(),
+                status: String::from(status),
+            };
+            subscriber.update(payload);
+        }
+    }
 }
